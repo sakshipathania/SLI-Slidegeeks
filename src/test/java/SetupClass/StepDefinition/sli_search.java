7a31503678d80bdf287e248f625075d948369424
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-09-15 17:16:45 +0530

    Update sli_search.java

--- src/test/java/SetupClass/StepDefinition/sli_search.java
+++ src/test/java/SetupClass/StepDefinition/sli_search.java
@@ -52,7 +52,7 @@ public class sli_search extends Set{
 	@Then("^enter a keyword to search\\.$")
 	public void enter_a_keyword_to_search() throws Throwable {
 		
-		 for(int i=0; i<=23; i++) {
+		 for(int i=0; i<=24; i++) {
 
 		  WebElement search_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#search")));
 		  Thread.sleep(2000);
