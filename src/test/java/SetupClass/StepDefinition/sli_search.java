76613b9aa461b84f2278a897375fbb602041ae80
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-12-22 18:23:14 +0530

    Update sli_search.java

--- src/test/java/SetupClass/StepDefinition/sli_search.java
+++ src/test/java/SetupClass/StepDefinition/sli_search.java
@@ -211,6 +211,18 @@ public class sli_search extends Set{
 	    }
 	
 		  driver.navigate().refresh(); Thread.sleep(3800);
+		try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				Thread.sleep(1000);
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
+
+		}
 	}
 		
 		
