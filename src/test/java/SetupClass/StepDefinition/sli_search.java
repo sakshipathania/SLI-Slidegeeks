89d94c65980bd3f0174f8287f4ccd6d0f2cba30a
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-01-14 13:13:59 +0530

    Update sli_search.java

--- src/test/java/SetupClass/StepDefinition/sli_search.java
+++ src/test/java/SetupClass/StepDefinition/sli_search.java
@@ -55,41 +55,41 @@ public class sli_search extends Set{
 		
 		for(int i=0; i<=4; i++) {
 	
-		  WebElement search_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#search")));
+		  WebElement search_btn = wait.until(ExpectedConditions.elementToBeClickable(By.id("search-input")));
 		  Thread.sleep(2000);
 		  search_btn.sendKeys("Org Chart"); 
 		  Thread.sleep(4000);
 		  
-		  WebElement search_1 =wait.until(ExpectedConditions.elementToBeClickable(By.id("sli_ac_section_products"))); 
+		  WebElement search_1 =wait.until(ExpectedConditions.elementToBeClickable(By.className("sli_ac_products"))); 
 		  Thread.sleep(2000); 
 		  String text1=search_1.getText(); 
 		  System.out.println(text1); 
 		  Thread.sleep(3000);
 		  
-		  String actual_text1="Product Suggestions for Hr Interview ";
+		  String actual_text1="Product Suggestions for Org Chart Interview ";
 		  
 		  if(text1.equalsIgnoreCase(actual_text1)) {
 		  log.info("product suggestion tab header is displayed correctly "); }
 		  
-		  WebElement prod_sugg =wait.until(ExpectedConditions.elementToBeClickable(By.id("sli_raclist_products"))); 
+		  WebElement prod_sugg =wait.until(ExpectedConditions.elementToBeClickable(By.className("sli_ac_section"))); 
 		  Thread.sleep(2000); 
 		  String pro_sugg_loc=prod_sugg.getText();
 		  System.out.println("product suggestion list name is ----"+pro_sugg_loc);
 		  
-		  WebElement parent_product =wait.until(ExpectedConditions.elementToBeClickable(By.className("sli_product_list"))); 
+		  WebElement parent_product =wait.until(ExpectedConditions.elementToBeClickable(By.className("productList"))); 
 		  Thread.sleep(2000);
 		  System.out.println("classname  of parent is ----------------------->"+parent_product);
 		
 		  Thread.sleep(4000);
 	
-		  WebElement search_btn1 = driver.findElement(By.cssSelector("#search"));
+		  WebElement search_btn1 = driver.findElement(By.id("search-input"));
 		  Thread.sleep(2000);
 		  search_btn1.sendKeys(Keys.CONTROL + "a");
                   search_btn1.sendKeys(Keys.DELETE);
 		  Thread.sleep(8000);
 		
 		
-		if(driver.findElement(By.id("sli_raclist_products")).isDisplayed()){
+		if(driver.findElement(By.className("sli_ac_section")).isDisplayed()){
 			
                     System.out.println("Element is Visible");
 			throw new IOException ("Element Should not be visible");
